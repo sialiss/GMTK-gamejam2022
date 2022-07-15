45db39a8be3051a30cda65e2868a3e6d72aa67cd
och-och <och.hco.och.hco@gmail.com>
2022-07-16 00:34:50 +0300

    add StatusMachina

---
+++ utils/StatusMachina/StatusMachina.gd
@@ -0,0 +1,19 @@
+class_name StatusMachina
+
+# Returs the current status or null
+static func find(node: Node):
+	if node.has_meta("status"):
+		return node.get_meta("status")
+	return null
+
+static func detach(node: Node):
+	var old_status = find(node)
+	if old_status:
+		node.remove_child(old_status)
+		node.remove_meta("status")
+
+static func attach(node: Node, status):
+	status.host = node
+	detach(node)
+	node.set_meta("status", status)
+	node.add_child(status)
