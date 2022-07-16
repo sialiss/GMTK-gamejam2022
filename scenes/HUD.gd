06170f9bc9c1d92c8c0851661b9c02951f06d170
Camellia <56206613+sialiss@users.noreply.github.com>
2022-07-16 23:10:36 +0300

    Fix start game

--- scenes/HUD.gd
+++ scenes/HUD.gd
@@ -9,36 +9,36 @@ func _ready():
 	pass # Replace with function body.
 
 func show_message(text):
-    $Message.text = text
-    $Message.show()
-    $MessageTimer.start()
+	$Message.text = text
+	$Message.show()
+	$MessageTimer.start()
 
 func show_game_over():
-    show_message("Game Over")
-    # Wait until the MessageTimer has counted down.
-    yield($MessageTimer, "timeout")
+	show_message("Game Over")
+	# Wait until the MessageTimer has counted down.
+	yield($MessageTimer, "timeout")
 
-    $Message.text = "Dodge the\nCreeps!"
-    $Message.show()
-    # Make a one-shot timer and wait for it to finish.
-    yield(get_tree().create_timer(1), "timeout")
-    $StartButton.show()
+	$Message.text = "Dodge the\nCreeps!"
+	$Message.show()
+	# Make a one-shot timer and wait for it to finish.
+	yield(get_tree().create_timer(1), "timeout")
+	$StartButton.show()
 
 func update_score(score):
-    $ScoreLabel.text = str(score)
+	$ScoreLabel.text = str(score)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
 func _on_StartButton_pressed():
-    $StartButton.hide()
-    $Credits.hide()
-    $Gameochka.hide()
-    emit_signal("start_game")
+	$StartButton.hide()
+	$CreditsButton.hide()
+	$Gameochka.hide()
+	emit_signal("start_game")
 
 func _on_MessageTimer_timeout():
-    $Message.hide()
+	$Message.hide()
 
 func _on_ExitButton_pressed():
 	emit_signal("close_game")
