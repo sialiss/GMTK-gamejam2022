1d0297894aa621ad5979a88cc8526cc95c144dba
och-och <och.hco.och.hco@gmail.com>
2022-07-17 03:43:19 +0300

    Relocate CubeDisplay under Player/

--- scenes/Player/Dice/CubeDisplay3D.gd
+++ scenes/Player/Dice/CubeDisplay3D.gd
@@ -14,8 +14,19 @@ func rotate_down():
 	_turn(Vector3.RIGHT, TAU/4)
 
 func _turn(axel: Vector3, angle: float):
-	target_transform = target_transform.rotated(axel, angle)
+	target_transform = target_transform.rotated(axel, angle).orthonormalized()
 	if tween:
 		tween.kill()
 	tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
 	tween.tween_property(mesh, "transform", target_transform, 0.5)
+
+# Returns the number on the top side of the dice
+func get_number():
+	var basis = target_transform.basis
+	if ( basis.z).is_equal_approx(Vector3.UP): return 1
+	if ( basis.x).is_equal_approx(Vector3.UP): return 2
+	if ( basis.y).is_equal_approx(Vector3.UP): return 3
+	if (-basis.x).is_equal_approx(Vector3.UP): return 4
+	if (-basis.y).is_equal_approx(Vector3.UP): return 5
+	if (-basis.z).is_equal_approx(Vector3.UP): return 6
+	return 0
