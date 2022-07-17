677bc144e52abdf51427da3ef1bc53200171d3fe
Camellia <56206613+sialiss@users.noreply.github.com>
2022-07-17 19:33:46 +0300

    softening the change in hp

--- scenes/Player/Health.gd
+++ scenes/Player/Health.gd
@@ -9,4 +9,7 @@ func _ready():
 
 func move_pawn(change: float):
 	var step = change * 4.8
-	$Pawn.global_position.x = clamp($Pawn.global_position.x + step, $StartPosition.global_position.x, $EndPosition.global_position.x)
+	var new_position = $Pawn.global_position + Vector2(step, 0)
+	new_position.x = clamp(new_position.x, $StartPosition.global_position.x, $EndPosition.global_position.x)
+	var tween = create_tween().set_ease(Tween.EASE_OUT)
+	tween.tween_property($Pawn, "global_position", new_position, 0.2)
